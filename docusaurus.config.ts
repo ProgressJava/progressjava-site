7f4eebc0b33594b9dc3a1fef2eac321afd2b4a66
Suren K <thesurenk@gmail.com>
2025-10-14 15:31:55 -0500

    fixed a typo on home page

--- docusaurus.config.ts
+++ docusaurus.config.ts
@@ -5,7 +5,7 @@ import type * as Preset from '@docusaurus/preset-classic';
 // This runs in Node.js - Don't use client-side code here (browser APIs, JSX...)
 
 const config: Config = {
-  title: 'Welcome toProgress Java',
+  title: 'Welcome To Progress Java',
   tagline: 'The open-source guide to modern Java fundamentals, clean examples, and updates with every JDK release.',
   favicon: 'img/favicon.ico',
 
